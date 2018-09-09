ee6a3e57be57aa6f60719f9a77c3c5a916fe5a77
sarver767 <43116251+sarver767@users.noreply.github.com>
2018-09-09 14:25:38 -0500

    Update BlueDestroyer.py

--- BlueDestroyer.py
+++ BlueDestroyer.py
@@ -1,12 +1,17 @@
 # BlueDestroyer
 
-#Declare Stuff
+# Declare Stuff
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import chalk
 import os
+import random
+
+# Create Response List
+response = ['You will be contained!', 'Get your gayness outta here!', 'Sucken dick I see...', 'Stop linking gay shit', 'One day you will be no more.']
+insult = random.SystemRandom()
 
 # Create Bot Commands
 bot = commands.Bot(command_prefix='#')
@@ -23,7 +28,7 @@ async def on_ready():
 async def on_message(message):
     if message.content[-8:] == "cat3.jpg":
         await bot.delete_message(message)
-        await bot.send_message(message.author, 'You will be contained!!!!!!!!!!')
+        await bot.send_message(message.author, insult.choice(response)
         await bot.process_commands(message)
 
 
