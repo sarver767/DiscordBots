887ad853b47132a99bfe67d76cf95cbb3f252d8e
sarver767 <43116251+sarver767@users.noreply.github.com>
2018-09-09 11:55:00 -0500

    Add files via upload

---
+++ BlueDestroyer.py
@@ -0,0 +1,29 @@
+# BlueDestroyer
+
+#Declare Stuff
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+# Create Bot Commands
+bot = commands.Bot(command_prefix='#')
+
+# Prep Bot
+@bot.event
+async def on_ready():
+    print ("Blue will be contained.")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+
+
+@bot.event
+async def on_message(message):
+    if message.content[-8:] == "cat3.jpg":
+        await bot.delete_message(message)
+        await bot.send_message(message.author, 'You will be contained!!!!!!!!!!')
+        await bot.process_commands(message)
+
+
+bot.run("NDg4MjA2NzQ0MTkyMDI0NTk5.DnY-BQ.eBMsmljqgJb7PbeSJFk8bnGtOWE")
