03229796efd2dbfc81ba07ca654a28104ae2db31
sarver767 <43116251+sarver767@users.noreply.github.com>
2018-09-09 21:11:25 -0500

    Update BlueDestroyer.py

--- BlueDestroyer.py
+++ BlueDestroyer.py
@@ -17,7 +17,7 @@ response = ['You will be contained!', 'Get your gayness outta here!', 'Sucken di
 insult = random.SystemRandom()
 
 # Create Coin
-coin = ['/Heads.gif', '/Tails.gif']
+coin = ['/app/Heads.gif', '/app/Tails.gif']
 side = random.SystemRandom()
 
 # Create Bot Commands
